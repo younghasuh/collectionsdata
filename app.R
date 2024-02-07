68c5d515f9b7dd92efbb21818f07cb1fcbb9dc67
younghasuh <ys757@cornell.edu>
2024-02-06 18:05:59 -0800

    Updating features - weight boxplot is interactive - leaflet map points are clickable

--- app.R
+++ app.R
@@ -7,6 +7,7 @@ library(urbnmapr)
 library(usmap)
 library(leaflet)
 library(sf)
+library(ggiraph)
 
 # set working directory
 here::i_am("app.R")
@@ -17,6 +18,11 @@ data <- read.csv("data_2023.csv")
 # List of species names for autofill field
 autocomplist <- data$species
 
+# Load additional tidying data sets
+specnat <- read.csv(here("specnat.csv"))
+sta <- read.csv(here("states.csv"))
+
+#####
 # Set UI (user interface)
 
 ui <- fluidPage(
@@ -25,16 +31,20 @@ ui <- fluidPage(
   titlePanel(title =  div(img(src="NHM_logo_black_250.png", 
                               width="50px", height="50px"), 
                           "LACM Specimen trends and maps")),
-  
+  # panel with tabs 
   mainPanel(
     tabsetPanel(
       type = "tabs",
       
+      ###
       # tab 1
       tabPanel(
         titlePanel("Species summary"),
         
+        # using panel with side bar
         sidebarLayout(
+          
+          # set up side bar panel
           sidebarPanel(
             h2("Species"),
             p("Start typing species and select from drop-down list."),
@@ -50,6 +60,7 @@ ui <- fluidPage(
             fluidRow(column(12, h4("Specimen count by specimen type/nature"), tableOutput("specnat")))
           ),
           
+          # set up main panel 
           mainPanel(
             
             fluidRow(column(12, h4("Specimen count by year"), plotOutput("trend"))),
@@ -62,7 +73,10 @@ ui <- fluidPage(
             
             fluidRow(column(12, h4("Global specimen distribution"), leafletOutput(outputId = 'map'))),
             
-            fluidRow(column(12, h4("Weights by sex"), plotOutput(outputId = "wtPlot"))),
+            fluidRow(column(8, h4("Weights by sex - interactive"), girafeOutput("wtPlot2")),
+                     column(4, h4("Hovering points"), verbatimTextOutput("console"),
+                            h4("Selected points"), tableOutput("datatab"))),
+            
             
             fluidRow(h4("Detailed counts by year"),
                      column(2, tableOutput("countbyyear")),
@@ -70,7 +84,9 @@ ui <- fluidPage(
           )
         )),
       
+      ###
       # tab 2
+      # only using single panel
       tabPanel(
         titlePanel("LACM lookup"),
         textInput("catalog", "LACM"),
@@ -80,32 +96,23 @@ ui <- fluidPage(
       
     )))
 
+
 #####
 # Define server
 server <- function(input, output, session) {
   
+  # for autofill list; put on server side to load app faster 
   updateSelectizeInput(session, "sp", choices = autocomplist, selected=character(0), server = TRUE)
   
+  # filter data for species selected in the UI 
   selected <- reactive(data %>% filter(species == input$sp))
+
+  # Table for specimen total count by specimen type
+  output$specnat <- renderTable(
+    selected() %>% count(Description)
+  )
   
-  output$wtPlot <- renderPlot({
-    selected() %>% 
-      ggplot(aes(x=sex, y=wt.j)) +
-      stat_boxplot(geom="errorbar", position="dodge2") +
-      geom_boxplot(stat = "boxplot",
-                   position = "dodge2") + 
-      geom_point(shape=16, alpha=0.4) +
-      theme_minimal() +
-      scale_x_discrete(limits = c("M", "F", "U"), labels = c("Male", "Female", "Unknown")) +
-      labs(x = "Sex", y = "Weight (g)")
-  })
-  
-  #position=position_jitter(0.2), 
-  
-  output$wtdata <- renderTable({
-    nearPoints(selected(), input$plot_click, allRows = F)
-  }) # this works but doesn't want to maintain when there are 2+ points with the same y value
-  
+  # from the selected data, filter out skels and skins
   data_filt <- reactive({
     selected() %>% 
       filter(nat == "skeleton" | nat == "study skin")
@@ -186,18 +193,21 @@ server <- function(input, output, session) {
   })
   
   # leaflet map
+  # filter data for map data
   map_df <- reactive({
     selected() %>% 
       filter(!is.na(lng) & !is.na(lat)) %>% 
       st_as_sf(coords = c("lng", "lat"))
   })
   
+  # actual map
   output$map = renderLeaflet({
     leaflet() %>%
       addTiles() %>%
       addCircleMarkers(data = map_df(), radius=1) 
   })  
   
+  # boxplot with weights split by sex
   output$wtPlot <- renderPlot({
     selected() %>% 
       ggplot(aes(x=sex, y=wt)) +
@@ -210,9 +220,54 @@ server <- function(input, output, session) {
       labs(x = "Sex", y = "Weight (g)")
   })
   
+  # with ggiraph for interactive plot 
+  selected_pts <- reactive({
+    input$wtPlot2_selected
+  })
+  
+  output$console <- renderPrint({
+    input$wtPlot2_hovered
+  })
+  
+  output$wtPlot2 <- renderGirafe({
+    gg_bx <- ggplot(selected(), aes(x=sex, y=wt)) +
+      stat_boxplot(geom="errorbar", position="dodge2") +
+      geom_boxplot(stat="boxplot", position="dodge2", outlier.shape = NA) +
+      geom_point_interactive(aes(tooltip=lacm, data_id=lacm),
+                             size=3, hover_nearest=T, position=position_jitter(0.2))  +
+      scale_x_discrete(limits = c("M", "F", "U"), labels = c("Male", "Female", "Unknown")) +
+      labs(x = "Sex", y = "Weight (g)")
+    girafe(ggobj = gg_bx)
+  })
+  
+  output$datatab <- renderTable({
+    #  selected_pts() 
+    out <- selected()[selected()$lacm %in% selected_pts(),] %>% 
+      mutate(LACM = lacm, LAF = laf, Sex = sex, subspecies = spp, Date = datecoll, Locality = locality, SpecType = nat) %>% 
+      select(LACM, LAF, Sex, subspecies, Date, Locality, SpecType) 
+    if( nrow(out) < 1 ) return(NULL)
+    row.names(out) <- NULL
+    out
+  })
+  
+  
+  
+  # Table for specimen count by year
+  output$countbyyear <- renderTable(
+    selected() %>% count(year)
+  )
+  
+  # Table for specimen count by year by specimen type
+  output$summary <- renderTable(
+    selected() %>% count(year, specnat)
+  )
+  
+  
   ##### Tab 2
+  # new filter based on catalog number (LACM)
   selected2 <- reactive(data %>% filter(lacm == input$catalog)) 
   
+  # table listing all relevant field information 
   output$catcount <- renderTable(
     selected2() %>% 
       mutate(
@@ -228,20 +283,21 @@ server <- function(input, output, session) {
       select(LACM, LAF, Family, Species, Subspecies, Sex, Date, Description, Locality)
   )
   
-  
-  
-  # using leaflet instead
+  ###
+  # leaflet map of locality
+  # set up filtered data, remove any NA values (will not plot)
   catmap_df <- reactive({
     selected2() %>% 
       filter(!is.na(lng) & !is.na(lat)) %>% 
       st_as_sf(coords = c("lng", "lat"))
   })
   
+  # set up leaflet map 
   output$catmap = renderLeaflet({
     leaflet() %>%
       addTiles() %>%
-      addCircleMarkers(data = catmap_df(), radius=1)
-    
+      addCircleMarkers(data = catmap_df(), radius=1, 
+                       popup=paste("LACM ", catmap_df()$lacm, "<br>", catmap_df()$datecoll, sep = " "))
   })
   
 }
