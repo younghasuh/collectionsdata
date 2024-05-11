5aa213e35e45f07257d44c658a16ae9cb5eb9bc9
younghasuh <ys757@cornell.edu>
2024-05-10 18:01:28 -0700

    Updated script

--- server.R
+++ server.R
@@ -1,3 +1,6 @@
+# server.R
+
+library(plotly)
 library(shiny)
 library(ggplot2)
 library(tidyverse)
@@ -8,26 +11,78 @@ library(leaflet)
 library(sf)
 library(ggiraph)
 library(htmltools)
+library(shinydashboard)
+library(DT)
 
-data <- read.csv("data_2023.csv")
-autocomplist <- data$species
-specnat <- read.csv(here("specnat.csv"))
-sta <- read.csv(here("states.csv"))
+
+# Load data
+data <- read.csv("data_20240328.csv")
+alist <- sort(unique(unlist(data$species, use.names = FALSE)))
+ind <- c("sex", "spp", "state", "month")
+
 
 shinyServer(function(input, output, session) {
   
-  # for autofill list; put on server side to load app faster 
-  updateSelectizeInput(session, "sp", choices = autocomplist, selected=character(0), server = TRUE)
+  ### TAB 1
+  # using selectize input; putting autofill list on server side to reduce processing speed
+  updateSelectizeInput(session, "sp", choices = alist, selected=character(0), server = TRUE)
   
-  # filter data for species selected in the UI 
-  selected <- reactive(data %>% filter(species == input$sp))
+  # reactive input for species for tab 1
+  selected <- reactive(data %>% filter(species == req(input$sp)))
   
-  # Table for specimen total count by specimen type
+  
+  # table for specimen nature 
   output$specnat <- renderTable(
     selected() %>% count(Description)
   )
   
-  # from the selected data, filter out skels and skins
+  # table for sex
+  output$sexcount <- renderTable(
+    selected() %>% count(Sex)
+  )
+  
+  # table for age
+  output$agecount <- renderTable(
+    selected() %>% count(age)
+  )
+  
+  # table for list
+  output$spectab <- renderDT({
+    dat <- selected() %>% 
+      mutate(
+        LACM = lacm,
+        LAF = laf,
+        Order = order,
+        Family = family,
+        Species = species,
+        Subspecies = spp,
+        Date = datecoll,
+        Locality = locality
+      ) %>% 
+      select(LACM, LAF, Order, Family, Species, Subspecies, Sex, Date, Description, Locality)
+    
+    DT::datatable(dat,
+                  class = 'cell-border stripe',
+                  rownames = F,
+                  extensions = c("Buttons", "Select"),
+                  selection = 'none',
+                  options = 
+                    list(
+                      pageLength = 10, autoWidth = TRUE,
+                      dom = 'Bfrtip',
+                      select = TRUE,
+                      buttons = list(
+                        list(
+                          extend = "copy",
+                          text = 'Copy'#,
+                          #exportOptions = list(modifier = list(selected = TRUE))
+                        )
+                      )
+                    )) 
+  })
+  
+  
+  # filter only skeletons and study skins for simplified figures
   data_filt <- reactive({
     selected() %>% 
       filter(nat == "skeleton" | nat == "study skin")
@@ -44,18 +99,6 @@ shinyServer(function(input, output, session) {
     
   }, res = 96)
   
-  output$countbyyear <- renderTable(
-    selected() %>% count(year)
-  )
-  
-  output$summary <- renderTable(
-    selected() %>% count(year, specnat)
-  )
-  
-  output$specnat <- renderTable(
-    selected() %>% count(Description)
-  )
-  
   # count by month and type
   output$trend2 <- renderPlot({
     data_filt() %>% 
@@ -65,8 +108,12 @@ shinyServer(function(input, output, session) {
       theme_classic() +
       labs(fill = "Specimen type", color = "Specimen type", x = "Month", y = "Count")
     
-  }, res = 96)  
+  }, res = 96)
   
+  
+  
+  # reactive map by state
+  # US only
   spat_state1 <- reactive({
     left_join(get_urbn_map(map = "states", sf = TRUE),
               selected() %>% 
@@ -74,7 +121,6 @@ shinyServer(function(input, output, session) {
               by = c("state_abbv" = "abv"))
   })
   
-  # app format  
   output$state <- renderPlot({
     spat_state1() %>% 
       ggplot() +
@@ -96,7 +142,6 @@ shinyServer(function(input, output, session) {
   })
   
   
-  # app format  
   output$ca_cty <- renderPlot({
     spat_ca_cty () %>% 
       ggplot() +
@@ -107,37 +152,48 @@ shinyServer(function(input, output, session) {
       scale_fill_viridis_c(option = "D")  
   })
   
+  
   # leaflet map
-  # filter data for map data
+  # global
   map_df <- reactive({
     selected() %>% 
       filter(!is.na(lng) & !is.na(lat)) %>% 
       st_as_sf(coords = c("lng", "lat"))
   })
   
-  # actual map
   output$map = renderLeaflet({
     leaflet() %>%
       addTiles() %>%
-      addCircleMarkers(data = map_df(), radius=1) 
+      addCircleMarkers(data = map_df(), radius=1, 
+                       popup=paste("LACM ", map_df()$lacm, "<br>", map_df()$datecoll, sep = " ")) 
   })  
   
-  # boxplot with weights split by sex
+  ## tab 2 
+  # boxplot for weights
+  output$wtPlot <- renderPlot({
+    selected() %>% 
+      ggplot(aes(x=Sex, y=wt)) +
+      stat_boxplot(geom="errorbar", position="dodge2") +
+      geom_boxplot(stat = "boxplot",
+                   position = "dodge2") + 
+      geom_point(shape=16, alpha=0.4, position=position_jitter(0.2)) +
+      theme_minimal() +
+      scale_x_discrete(limits = c("M", "F", "U"), labels = c("Male", "Female", "Unknown")) +
+      labs(x = "Sex", y = "Weight (g)")
+  })
+  
+  ## tab 3
   # with ggiraph for interactive plot 
-  
-  # points that will be selected in the interactive plot
   selected_pts <- reactive({
-    input$wtPlot_selected
+    input$wtPlot2_selected
   })
   
-  # hovered points 
   output$console <- renderPrint({
-    input$wtPlot_hovered
+    input$wtPlot2_hovered
   })
   
-  # interactive plot
-  output$wtPlot <- renderGirafe({
-    gg_bx <- ggplot(selected(), aes(x=sex, y=wt)) +
+  output$wtPlot2 <- renderGirafe({
+    gg_bx <- ggplot(selected(), aes(x=Sex, y=wt)) +
       stat_boxplot(geom="errorbar", position="dodge2") +
       geom_boxplot(stat="boxplot", position="dodge2", outlier.shape = NA) +
       geom_point_interactive(aes(tooltip=lacm, data_id=lacm),
@@ -147,63 +203,83 @@ shinyServer(function(input, output, session) {
     girafe(ggobj = gg_bx)
   })
   
-  # table of all the selected points from the interactive plot
   output$datatab <- renderTable({
+    #  selected_pts() 
     out <- selected()[selected()$lacm %in% selected_pts(),] %>% 
-      mutate(LACM = lacm, LAF = laf, Sex = sex, subspecies = spp, Date = datecoll, Locality = locality, SpecType = nat) %>% 
+      mutate(LACM = lacm, LAF = laf, subspecies = spp, Date = datecoll, Locality = locality, SpecType = nat) %>% 
       select(LACM, LAF, Sex, subspecies, Date, Locality, SpecType) 
     if( nrow(out) < 1 ) return(NULL)
     row.names(out) <- NULL
     out
   })
   
+  ## tab 4
+  # plotly boxplots - variable y
+  x <- reactive({
+    x <- selected()[[input$xaxis]]
+  })
   
-  # Table for specimen count by year
-  output$countbyyear <- renderTable(
-    selected() %>% count(year)
-  )
+  output$plot <- renderPlotly({
+    dat <- selected()
+    plot_ly(dat, x = x(), y = dat$wt, type = "box",
+            boxpoints = "all", jitter = 0.8,
+            pointpos = 0, marker = list(size = 3),
+            hoverinfo = "text",
+            text = ~paste("LACM:", dat$lacm, ";", "Weight:", dat$wt, sep=" "))  %>%
+      layout(boxmode = "group",
+             xaxis = list(title='Grouping'),
+             yaxis = list(title='Weight (g)'))
+  })
   
-  # Table for specimen count by year by specimen type
-  output$summary <- renderTable(
-    selected() %>% count(year, specnat)
-  )
   
-  
-  ##### Tab 2
-  # new filter based on catalog number (LACM)
+  #### TAB 2 ----
   selected2 <- reactive(data %>% filter(lacm == input$catalog)) 
   
-  # table listing all relevant field information 
   output$catcount <- renderTable(
     selected2() %>% 
       mutate(
         LACM = lacm,
         LAF = laf,
+        Order = order,
         Family = family,
         Species = species,
         Subspecies = spp,
-        Sex = sex,
         Date = datecoll,
         Locality = locality
       ) %>% 
-      select(LACM, LAF, Family, Species, Subspecies, Sex, Date, Description, Locality)
+      select(LACM, LAF, Order, Family, Species, Subspecies, Sex, Date, Description, Locality)
   )
   
-  ###
-  # leaflet map of locality
-  # set up filtered data, remove any NA values (will not plot)
+  
   catmap_df <- reactive({
     selected2() %>% 
       filter(!is.na(lng) & !is.na(lat)) %>% 
       st_as_sf(coords = c("lng", "lat"))
   })
   
-  # set up leaflet map 
   output$catmap = renderLeaflet({
     leaflet() %>%
       addTiles() %>%
       addCircleMarkers(data = catmap_df(), radius=1, 
                        popup=paste("LACM ", catmap_df()$lacm, "<br>", catmap_df()$datecoll, sep = " "))
+    
   })
+  
+  
+  #### TAB 3 ----
+  output$lacmsumm <- renderTable(
+    max(data$lacm),
+    rownames = F, colnames = F
+  )
+  
+  output$toptable <- renderDT({
+    gettab <- data %>% count(get(input$category))
+    
+    DT::datatable(gettab,
+                  class = 'cell-border stripe',
+                  rownames = F,
+                  colnames = c("", "N"))
+  })
+  
   
 })
--- ui.R
+++ ui.R
@@ -1,27 +1,28 @@
+library(plotly)
 library(shiny)
 library(leaflet)
 library(ggiraph)
+library(htmltools)
+library(shinydashboard)
+library(tidyverse)
+library(DT)
 
-shinyUI(fluidPage(
+
+shinyUI(
   
-  # title of the app, including a logo image
-  titlePanel(title =  div(img(src="NHM_logo_black_250.png", 
-                              width="50px", height="50px"), 
-                          "LACM Specimen trends and maps")),
-  # panel with tabs 
-  mainPanel(
+  fluidPage(
+    titlePanel(title =  div(img(src="NHM_logo_black_250.png", width="50px", height="50px"), 
+                            "LACM Collection Information"), windowTitle = "LACM Collection Information"),
+    p("Updated Oct 2023"),
+    
     tabsetPanel(
       type = "tabs",
       
-      ###
-      # tab 1
+      # tab 1 ---
       tabPanel(
         titlePanel("Species summary"),
         
-        # using panel with side bar
         sidebarLayout(
-          
-          # set up side bar panel
           sidebarPanel(
             h2("Species"),
             p("Start typing species and select from drop-down list."),
@@ -32,44 +33,65 @@ shinyUI(fluidPage(
               choices = NULL,
               selected = NULL,
               multiple = FALSE, # allow for multiple inputs
-              options = NULL),
-            
-            fluidRow(column(12, h4("Specimen count by specimen type/nature"), tableOutput("specnat")))
-          ),
+              options = NULL)),
           
-          # set up main panel 
+          # main panel for displaying outputs for the selectize Input (species name)
           mainPanel(
             
-            fluidRow(column(12, h4("Specimen count by year"), plotOutput("trend"))),
-            
-            fluidRow(column(12, h4("Specimen count by month"), plotOutput("trend2"))),
-            
-            fluidRow(column(12, h4("Specimen count by state (US only)"), plotOutput("state"))),
-            
-            fluidRow(column(12, h4("Specimen count by county (CA only)"), plotOutput("ca_cty"))),
-            
-            fluidRow(column(12, h4("Global specimen distribution"), leafletOutput(outputId = 'map'))),
-            
-            fluidRow(column(8, h4("Weights by sex - interactive"), girafeOutput("wtPlot2")),
-                     column(4, h4("Hovering points"), verbatimTextOutput("console"),
-                            h4("Selected points"), tableOutput("datatab"))),
-            
-            
-            fluidRow(h4("Detailed counts by year"),
-                     column(2, tableOutput("countbyyear")),
-                     column(3, tableOutput("summary")))
-          )
-        )),
+            tabsetPanel(type = "tabs",
+                        tabPanel(title = "Summary", 
+                                 fluidRow(column(12, h4("Specimen count by specimen type/nature"), tableOutput("specnat"))),
+                                 fluidRow(column(3, tableOutput("sexcount")),
+                                          column(3, tableOutput("agecount")))),
+                        
+                        tabPanel(title = "Count data", 
+                                 fluidRow(column(12, h4("Specimen count by year"), plotOutput("trend"))),
+                                 fluidRow(column(12, h4("Specimen count by month"), plotOutput("trend2"))),
+                                 fluidRow(column(12, h4("Specimen count by state (US only)"), plotOutput("state"))),
+                                 fluidRow(column(12, h4("Specimen count by county (CA only)"), plotOutput("ca_cty"))),
+                                 fluidRow(column(12, h4("Global specimen distribution"), leafletOutput(outputId = 'map'))),
+                                 fluidRow(column(12, h4("List of all specimens"), tableOutput("summary")))
+                        ),
+                        
+                        tabPanel(title = "Weights by sex",
+                                 fluidRow(column(8, h4("Weights by sex - interactive"), girafeOutput("wtPlot2")),
+                                          column(4, h4("Hovering points"), verbatimTextOutput("console"),
+                                                 h4("Selected points"), tableOutput("datatab")))
+                        ),
+                        
+                        tabPanel(title = "Data explorationg with Plotly",
+                                 fluidRow(column(12, h4("Plotly boxplots"), 
+                                                 box(selectInput("xaxis", "Select variable (x-axis)",
+                                                                 choices = c("sex", "spp", "state", "month"))))),
+                                 fluidRow(column(12, plotlyOutput("plot")))
+                        ),
+                        
+                        tabPanel(title = "Table of all specimens",
+                                 fluidRow(column(12, h4("List of all specimens. Use shift or ctrl to select multiple rows for copying onto clipboard."),
+                                                 DTOutput("spectab"))))
+            )
+          ) # mainpanel end
+        ) # sidebar layout end
+      ), # tabPanel end
       
-      ###
-      # tab 2
-      # only using single panel
+      # tab 2 ----
       tabPanel(
         titlePanel("LACM lookup"),
         textInput("catalog", "LACM"),
         fluidRow(column(12, tableOutput("catcount"))),
         fluidRow(column(12, h4("Leaftlet map"), leafletOutput(outputId = 'catmap')))
-      )
+      ),
       
-    )))
+      
+      # tab 3 ----
+      tabPanel(
+        titlePanel("Explore the collection"),
+        fluidRow(h4("Last updated 24 Oct 2023")),
+        fluidRow(column(12, h4("Total number of specimens"), tableOutput("lacmsumm"))),
+        fluidRow(column(12, selectInput("category", "Select category:",
+                                        choices = c("Description", "Sex", "order", "family", "genus", "year", "country")))),
+        fluidRow(column(12, DTOutput("toptable")))
+      )
+    )
+  )
 )
